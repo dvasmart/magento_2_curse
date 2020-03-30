4a379e866596caa6864d207b808eb48c00c6f32c
Vitalii Dronskii <dvasmart@gmail.com>
2020-03-30 17:57:27 +0300

    trying to add cms block with message

---
+++ app/code/Vitalii/Test/Setup/Patch/Data/MyCMSBlock.php
@@ -0,0 +1,89 @@
+<?php
+
+
+namespace Vitalii\Test\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Psr\Log\LoggerInterface;
+use Magento\Cms\Model\BlockFactory;
+
+/**
+ * Class MessageCMSBlock
+ */
+class MyCMSBlock implements DataPatchInterface
+{
+    /**
+     * @var BlockFactory
+     */
+    private $blockFactory;
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        LoggerInterface $logger,
+        BlockFactory $blockFactory
+    )
+    {
+        $this->blockFactory=$blockFactory;
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+
+        $this->moduleDataSetup->startSetup();
+
+
+        $data = [
+            'title' => 'Message for CMS block',
+            'identifier' => 'my_block',
+            'stores' => ['0'],
+            'is_active' => 1,
+            'content' => '<div class="cms-plp-page">
+                            <div class="message_for_cms_block">My Hello message for cms block</div>
+                          </div>
+            ',
+        ];
+        try {
+            $this->blockFactory->create()->setData($data)->save();
+        } catch (\Exception $exception) {
+            $this->logger->debug('Cannot insert block, message: "'. $exception->getMessage() . '"');
+        }
+
+        $this->moduleDataSetup->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
